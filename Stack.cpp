c1c067a4d48b71662a6e77320f57beb9be8a3e74
SathiVivekReddy123 <sathivivekreddykkd@gmail.com>
2025-07-18 09:45:33 +0530

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,15 @@
+#include"stack.h"
+using namespace std;
+int main(){
+    int st[n];
+    int top = -1;
+    push(st,top,11);
+    push(st,top,22);
+    push(st,top,33);
+    push(st,top,44);
+    push(st,top,55);
+    pop(st,top);
+    push(st,top,66);
+    push(st,top,77);
+    display(st,top);
+}
